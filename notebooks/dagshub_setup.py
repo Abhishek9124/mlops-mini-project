c8e072e399207fac84fd5d3747144b7e2b43b852
Abhishek9124 <gangurde.8278@gmail.com>
2025-12-13 15:34:57 +0530

    updated code

--- notebooks/dagshub_setup.py
+++ notebooks/dagshub_setup.py
@@ -1,7 +1,9 @@
 import dagshub
+import mlflow
+
+mlflow.set_tracking_uri('https://dagshub.com/Abhishek9124/mlops-mini-project.mlflow')
 dagshub.init(repo_owner='Abhishek9124', repo_name='mlops-mini-project', mlflow=True)
 
-import mlflow
 with mlflow.start_run():
   mlflow.log_metric('accuracy', 42)
   mlflow.log_param('Param name', 'Value')
